87619ca1b9436ea435bfe07aed270af819230ddf
Jockesss <71615580+jockesss@users.noreply.github.com>
2023-03-22 22:48:09 +1000

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,30 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin")
+
+public class AdminController {
+
+    @GetMapping()
+    public ModelAndView allUsers() {
+        return new ModelAndView("admin/admin");
+    }
+
+}
+
